b2bbdad4e2d7e10a5b30efa6b0dcea794347d032
ngynmikan <ngynmikan@gmail.com>
2023-06-26 09:07:38 +0700

    update getf() to use recursion

--- src/main/java/org/sea/mathutil/mvn/core/MathUtility.java
+++ src/main/java/org/sea/mathutil/mvn/core/MathUtility.java
@@ -13,18 +13,29 @@ public class MathUtility {
     
     public static final double PI = 3.1415;
     
+//    public static long getFactorial(int n) {
+//        
+//        long product = 1;
+//        
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n, n must be between 0 to 20, please.");
+//        
+//        if (n == 0 || n == 1)
+//            return 1;
+//        
+//        for (int i = 2; i <= n; i++)
+//            product *= i;
+//            return product;
+//    }
     public static long getFactorial(int n) {
-        
-        long product = 1;
-        
+ 
         if (n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n, n must be between 0 to 20, please.");
         
         if (n == 0 || n == 1)
             return 1;
         
-        for (int i = 2; i <= n; i++)
-            product *= i;
-            return product;
+        return n * getFactorial(n - 1);
+  
     }
 }
